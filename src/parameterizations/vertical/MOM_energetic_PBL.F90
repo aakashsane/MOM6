c025605ebfb8c3111ae9080a248823fe5a7e50ae
Aakash Sane <aakashsane@gmail.com>
2024-08-05 16:02:16 -0400

    fixed typo

--- src/parameterizations/vertical/MOM_energetic_PBL.F90
+++ src/parameterizations/vertical/MOM_energetic_PBL.F90
@@ -2681,7 +2681,7 @@ subroutine energetic_PBL_init(Time, G, GV, US, param_file, diag, CS)
    call get_param(param_file, mdl, "v0_lower_cap", CS%v0_lower_cap, &
                      "value of lower limit cap for v0, default is for 1 deg", units="m/s", default=2.5454E-06)
 
-   call get_param(param_file, mdl, "CS%f_lower", CS%CS%f_lower, &
+   call get_param(param_file, mdl, "CS%f_lower", CS%f_lower, &
                      "value of lower limit cap for Coriolis in v0", units="1/s", default=0.0001)
   !/ options end for Machine Learning Equation Discovery
 
